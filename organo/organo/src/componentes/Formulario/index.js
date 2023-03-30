e022d97480e921490a59c3dfeffa1c6c2ba83e4b
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-03-30 20:23:21 -0300

    estilização lista suspensa

--- organo/organo/src/componentes/Formulario/index.js
+++ organo/organo/src/componentes/Formulario/index.js
@@ -1,7 +1,16 @@
 import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css"
 
 const Formulario = ()=>{
+    const times=[
+        'Pogramação', 
+        'Front-end', 
+        'Data Science', 
+        'UX e Design', 
+        'Mobile', 
+        'Inovação e gestão'
+    ]
     return(
         <section className="formulario">
             <form>
@@ -9,6 +18,7 @@ const Formulario = ()=>{
             <CampoTexto label="Nome" placeholder="Digite o seu nome"/>
             <CampoTexto label="Cargo" placeholder="Digite o seu cargo"/>
             <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+            <ListaSuspensa label="Time"key={times} itens={times}/>
             </form>
         </section>
     )
