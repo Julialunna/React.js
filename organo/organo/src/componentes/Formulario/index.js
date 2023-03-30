14f2418fb6f021f477582ef76a8f90474f0efad4
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-03-30 20:50:00 -0300

    eventos com o botão

--- organo/organo/src/componentes/Formulario/index.js
+++ organo/organo/src/componentes/Formulario/index.js
@@ -12,14 +12,18 @@ const Formulario = ()=>{
         'Mobile', 
         'Inovação e gestão'
     ]
+    const aoSalvar=(evento)=>{
+        evento.preventDefault();
+        console.log("form submetido")
+    }
     return(
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
             <h2>Preencha os dados para criar o card do colaborador</h2>
-            <CampoTexto label="Nome" placeholder="Digite o seu nome"/>
-            <CampoTexto label="Cargo" placeholder="Digite o seu cargo"/>
+            <CampoTexto obrigatorio={true}label="Nome" placeholder="Digite o seu nome"/>
+            <CampoTexto obrigatorio={true}label="Cargo" placeholder="Digite o seu cargo"/>
             <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
-            <ListaSuspensa label="Time"key={times} itens={times}/>
+            <ListaSuspensa obrigatorio={true} label="Time"key={times} itens={times}/>
             <Botao>
                 Criar Card
             </Botao>
