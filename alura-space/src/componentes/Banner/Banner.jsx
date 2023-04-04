777c64f655e06a16893acce919d0056a54066b6c
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-04-04 14:02:34 -0300

    criando banner

--- alura-space/src/componentes/Banner/Banner.jsx
+++ alura-space/src/componentes/Banner/Banner.jsx
@@ -4,9 +4,9 @@ import styles from './Banner.module.scss';
 export default function Banner(){
 
     return(
-        <div className={styles.banner__container}>
+        <div className={styles.banner}>
             <img className={styles.img__banner} src={banner} alt="Imagem terra vista do espaço"/>
-            <h1>A galeria mais completa do espaço</h1>
+            <h1>A galeria mais completa de fotos do espaço!</h1>
         </div>
         /*<main>
             <section className={styles.principal}/>
--- alura-space/src/paginas/PaginaInicial/index.jsx
+++ alura-space/src/paginas/PaginaInicial/index.jsx
@@ -1,14 +1,17 @@
 import Banner from "../../componentes/Banner/Banner";
 import Cabecalho from "../../componentes/Cabecalho";
 import Menu from "../../componentes/Menu";
+import styles from './PaginaInicial.module.scss'
 
 
 export default function PaginaInicial(){
     return(
         <>
         <Cabecalho/>
-        <Menu/>
-        <Banner/>
+        <div className={styles.MenuEBanner}>
+            <Menu/>
+            <Banner/>
+        </div>
         </>
     )
 }
