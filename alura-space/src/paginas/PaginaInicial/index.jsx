b1b1536eac5d1e92e3617a032056a07c68d8cae5
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-04-04 16:56:21 -0300

    fazendo seção de populares

--- alura-space/src/paginas/PaginaInicial/index.jsx
+++ alura-space/src/paginas/PaginaInicial/index.jsx
@@ -2,6 +2,7 @@ import Banner from "../../componentes/Banner/Banner";
 import Cabecalho from "../../componentes/Cabecalho";
 import Galeria from "../../componentes/Galeria";
 import Menu from "../../componentes/Menu";
+import Populares from "../../componentes/Populares";
 import Rodape from "../../componentes/Rodape";
 import styles from './PaginaInicial.module.scss'
 
@@ -10,13 +11,16 @@ export default function PaginaInicial(){
     return(
         <>
         <Cabecalho/>
-        <div className={styles.MenuEBanner}>
-            <Menu/>
-            <Banner/>
-        </div>
-        <div>
-            <Galeria/>
-        </div>
+        <main>
+                <div className={styles.MenuEBanner}>
+                    <Menu/>
+                    <Banner/>
+                </div>
+            <div className={styles.galeria}>
+                <div className={styles.galeria1}><Galeria/></div>
+                <div className={styles.populares}><Populares/></div>
+            </div>
+        </main>
         <Rodape/>
         </>
     )
