01a391ca76495aeaef71e771e7557ac59fa90517
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-04-07 15:45:48 -0300

    fazendo botão

--- ola-mundo/src/paginas/NaoEncontrada/index.js
+++ ola-mundo/src/paginas/NaoEncontrada/index.js
@@ -1,6 +1,9 @@
 import styles from './NaoEncontrada.module.css';
 import erro404 from 'assets/erro_404.png'
+import BotaoPrincipal from 'componentes/BotaoPrincipal';
+import { useNavigate } from 'react-router-dom';
 export default function NaoEncontrada(){
+    const navegar=useNavigate();
     return(
         <>
         <div className={styles.conteudoContainer}> 
@@ -10,8 +13,8 @@ export default function NaoEncontrada(){
             Tem certeza de que era isso que você estava procurando?
             </p>
             <p className={styles.paragrafo}> Aguarde uns instantes e recarregue a página, ou volte para a página inicial. </p>
-            <div className={styles.botaoContainer}>
-                <button>Voltar</button>
+            <div className={styles.botaoContainer} onClick={()=> navegar(-1)}>
+                <BotaoPrincipal tamanho="lg">Voltar</BotaoPrincipal>
             </div>
                 <img className={styles.imagemCachorro} 
                 src={erro404} alt="Cachorro de óculos vestido como humano" />
