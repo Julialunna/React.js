231fd649f9ce3d6ca54ea23a360e80bc3fa4fcb0
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-04-07 10:50:30 -0300

    fazendo rodapé

--- ola-mundo/src/routes.js
+++ ola-mundo/src/routes.js
@@ -1,3 +1,4 @@
+import Rodape from "componentes/Rodape";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Menu from "./componentes/Menu";
 import Inicio from "./paginas/Inicio";
@@ -13,6 +14,7 @@ function AppRoutes() {
         <Route path='/sobremim' element={<SobreMim/>}/>
         <Route path="*" element={<div>Página não encontrada</div>}/>
       </Routes>
+      <Rodape/>
     </BrowserRouter>
   );
 }
