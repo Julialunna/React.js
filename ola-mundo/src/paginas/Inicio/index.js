4fb53e193cc6e68af4d99225e441887b1a77e99c
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-04-07 10:58:58 -0300

    usando rotas aninhadas

--- ola-mundo/src/paginas/Inicio/index.js
+++ ola-mundo/src/paginas/Inicio/index.js
@@ -3,18 +3,15 @@ import Post from 'componentes/Post';
 import styles from './Inicio.module.css';
 import posts from 'json/posts.json'
 
-export default function Inicio(){
-    return(
-        <main>
-            <Banner/>
-            <ul className={styles.posts}>
-                {posts.map((post)=>(
-                    <li key={post.id}>
-                        <Post post={post}/>
-                    </li>
-                )
-                )}
-            </ul>
-        </main>
+export default function Inicio() {
+    return (
+        <ul className={styles.posts}>
+            {posts.map((post) => (
+                <li key={post.id}>
+                    <Post post={post} />
+                </li>
+            )
+            )}
+        </ul>
     )
 }
