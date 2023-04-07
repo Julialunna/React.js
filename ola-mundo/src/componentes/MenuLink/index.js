e8078462d803868af791a705ca1c46c9b650c541
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-04-07 09:21:25 -0300

    fazendo desafio

--- ola-mundo/src/componentes/MenuLink/index.js
+++ ola-mundo/src/componentes/MenuLink/index.js
@@ -1,4 +1,4 @@
-import { Link, useLocation } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 import styles from './MenuLink.module.css'
 
 export default function MenuLink({children, to}) {
@@ -6,12 +6,13 @@ export default function MenuLink({children, to}) {
     const localizacao=useLocation();
 
     return (
-        <Link  className={`
-            ${styles.link} 
-            ${localizacao.pathname === to ? styles.linkDestacado : ""}
-        `}to={to}>
+        <NavLink  className={
+            ({isActive})=>`${styles.link}
+            ${isActive ? styles.linkDestacado : ""}`
+        }to={to}
+        end>
             {children}
-        </Link>
+        </NavLink>
     )
     
 }
