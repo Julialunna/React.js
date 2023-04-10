6766f124979c7b784e4531fef8503cd7a962a7e1
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-04-10 15:30:13 -0300

    iniciando projeto, fazendo cabeçalho e rodapé

---
+++ cinetag/src/components/Rodape/index.js
@@ -0,0 +1,10 @@
+import styles from './Rodape.module.css'
+
+function Rodape(){
+    return (
+        <footer className={styles.rodape}>
+            <p>Desenvolvido por Alura.</p>
+        </footer>
+    )
+}
+export default Rodape;
